f5d1a8a09827384f6c5f0acd7b07557d340ab450
EtihadStadium <mancity@mailbox.org>
2023-04-26 21:24:53 +0900

    Refactor websocket code

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,4 +1,6 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
 socket.addEventListener("open", () => {
     console.log("✅ You're connected to the server!");
 });
@@ -8,6 +10,9 @@ socket.addEventListener("message", (message) => {
 socket.addEventListener("close", () => {
     console.log("❌ Disconnected from the server!");
 });
-setTimeout(() => {
-    socket.send("Hello!");
-}, 10000);
+messageForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(input.value);
+    input.value = "";
+});
